ad0261a105a2cc2c5f08971cbcf02c012262c00c
Yuki Yoshida <worthmine@gmail.com>
2017-03-25 23:12:37 +0900

    add sub make_doing and insert it to sub join_noun

--- lib/Text/MeCab/More.pm
+++ lib/Text/MeCab/More.pm
@@ -50,7 +50,7 @@ sub parse {
         if( $node->{feature}[0] eq '接頭詞' and $next_feature[0] eq '名詞' ) {
             $node = $self->join_prefix( $node, \@parsed, $i );
         }elsif( $node->{feature}[0] eq '名詞' ) {
-            if( $node->{feature}[1] eq '形容動詞語幹' ) {
+            if( $node->{feature}[1] eq '形容動詞語幹' and decode_utf8($next->surface) =~ /^(だろ|だっ|で|に|だ|な|なら)$/ ) {
                 $node = $self->make_adjectival_noun( $node, \@parsed, $i );
             }elsif( $node->{feature}[1] eq '副詞可能' ) {
                 carp "副詞可能名詞の検出: $next_feature[0] at $i";
@@ -63,25 +63,15 @@ sub parse {
                 $node->{feature}[8],
                 ];
                 $node->{cost} += $next->cost;
-            }elsif( $next_feature[0] eq '名詞' ) {
-                $node = $self->join_noun( $node, \@parsed, $i );
 =cut
-
             }elsif( $node->{feature}[1] =~ /^サ変/ ) {
                 if( $next_feature[0] eq '動詞' and $next_feature[4] =~ /^サ変/ ) {
-                    $node->{pos} = '動詞';
-                    $node->{feature} = [ '動詞', 'サ変動詞', '*', '*', '*',
-                    $next_feature[5],
-                    $node->{feature}[6] . $next_feature[6],
-                    $node->{feature}[7] . $next_feature[7],
-                    $node->{feature}[8] . $next_feature[8],
-                    ];
-                    $node->{cost} += $next->cost;
-                    $node->{surface} .= decode_utf8($next->surface);
-                    splice @parsed, $i, 1;
-                    $node->{next} = $next->next;
+                    $node = $self->make_doing( $node, \@parsed, $i );
                 }
             }elsif( $next_feature[0] eq '名詞' and $next_feature[1] eq '接尾' ){
+                warn encode_utf8($next_feature[1].$node->{surface} );
+                $node = $self->join_noun( $node, \@parsed, $i );
+            }elsif( $next_feature[0] eq '名詞' and $next_feature[2] =~ /^(:?サ変|形容動詞語幹)/ ) {
                 $node = $self->join_noun( $node, \@parsed, $i );
             }
         }elsif( $node->{feature}[0] eq '動詞' ) {
@@ -144,6 +134,29 @@ sub join_verb {
     return $node;
 }
 
+sub make_doing {
+    my $self = shift;
+    my $node = shift;
+    my $parsed = shift;
+    my $i = shift;
+    my $next = $node->{next};
+    my @next_feature = split ',', decode_utf8($next->feature);
+    return $node if $next_feature[4] ne 'サ変・スル';
+
+    $node->{pos} = '動詞';
+    $node->{feature} = [ '動詞', 'サ変動詞', '*', '*', '*',
+        $next_feature[5],
+        $node->{feature}[6] . $next_feature[6],
+        $node->{feature}[7] . $next_feature[7],
+        $node->{feature}[8] . $next_feature[8],
+    ];
+    $node->{cost} += $next->cost;
+    $node->{surface} .= decode_utf8($next->surface);
+    splice $parsed, $i, 1;
+    $node->{next} = $next->next;
+    return $node;
+}
+
 sub make_adjectival_noun {
     my $self = shift;
     my $node = shift;
@@ -173,28 +186,32 @@ sub join_noun {
     my $i = shift;
     my $next = $node->{next};
     my @next_feature = split ',', decode_utf8($next->feature);
-    return $node if $next_feature[0] ne '名詞';
+    return $node if $next_feature[0] !~ /(:?名詞|動詞)/;
     return $node if $next_feature[1] =~ /^(:?代名詞|固有名詞)$/;
+    warn "debug: " . $next_feature[1] . $next_feature[0] . $node->{surface};
+    if( $next_feature[0] eq '名詞' ){
+        $node->{feature} = [ '名詞',
+            $node->{feature}[1] eq '*'? $next_feature[1] : $node->{feature}[1],
+            $node->{feature}[2] eq '*'? $next_feature[2] : $node->{feature}[2],
+            $node->{feature}[3] eq '*'? $next_feature[3] : $node->{feature}[3],
+            $node->{feature}[4] eq '*'? $next_feature[4] eq '*'? '熟語': '*' : $node->{feature}[4],
+            $node->{feature}[5] eq '*'? $next_feature[5] : $node->{feature}[5],
+            $node->{feature}[6] . $next_feature[6],
+            $node->{feature}[7] . $next_feature[7],
+            $node->{feature}[8] . $next_feature[8],
+        ];
+        $node->{surface} .= decode_utf8($next->surface);
+        splice $parsed, $i, 1;
+        $node->{next} = $next->next;
+    }
 
-    #return $node if $next_feature[2] eq '形容動詞語幹'
-    $node->{feature} = [ '名詞',
-        $node->{feature}[1] eq '*'? $next_feature[1] : $node->{feature}[1],
-        $node->{feature}[2] eq '*'? $next_feature[2] : $node->{feature}[2],
-        $node->{feature}[3] eq '*'? $next_feature[3] : $node->{feature}[3],
-        $node->{feature}[4] eq '*'? $next_feature[4] eq '*'? '熟語': '*' : $node->{feature}[4],
-        $node->{feature}[5] eq '*'? $next_feature[5] : $node->{feature}[5],
-        $node->{feature}[6] . $next_feature[6],
-        $node->{feature}[7] . $next_feature[7],
-        $node->{feature}[8] . $next_feature[8],
-    ];
-    $node->{surface} .= decode_utf8($next->surface);
-    splice $parsed, $i, 1;
-    $node->{next} = $next->next;
     my @next_next = split ',', decode_utf8($next->feature);
     if( $node->{feature}[2] eq '形容動詞語幹' ){
         $node = $self->make_adjectival_noun( $node, $parsed, $i );
     }elsif( $next_next[0] eq '名詞' ) {
         $node = $self->join_noun( $node, $parsed, $i );
+    }elsif( $next_next[0] eq '動詞' and $node->{feature}[2] =~ /^サ変/ and $next_next[4] eq 'サ変・スル' ) {
+        $node = $self->make_doing( $node, $parsed, $i );
     }
     return $node;
 }
